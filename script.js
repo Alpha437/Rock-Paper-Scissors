4a5d69786974959b4e6432a8c193d86fcaa4f645
Alpha437 <dapoade37@gmail.com>
2023-11-01 18:39:48 +0100

    Still got work to do

--- script.js
+++ script.js
@@ -14,10 +14,12 @@ const result = document.querySelector('.result');
 const resultText = document.querySelector('.result-text');
 const rulesBtn = document.querySelector('.rules');
 const replayBtn = document.querySelector('.replay');
+const hidden = document.querySelector('.hidden');
 
 // Destructuring
 const [playerMove, houseMove] = moveIcon;
 const [player, house] = userMove;
+let scoreCount = 0;
 
 let pMove, hMove;
 function resetGame() {
@@ -37,6 +39,8 @@ function collapseMoves() {
 function pickMove() {
   collapseMoves();
   const moveName = this.classList[1];
+  const hidden =
+    '<img src="./images/icon-paper.svg" alt="" class="paper-image user-move-icon" style="visibility: hidden;"></img>';
   const rps = ['rock', 'paper', 'scissors'];
   const randomMove = rps[Math.trunc(rps.length * Math.random())];
   let html;
@@ -46,33 +50,55 @@ function pickMove() {
   `;
   playerMove.classList.toggle(`${moveName}`);
   playerMove.innerHTML = html;
+  houseMove.innerHTML = hidden;
+  houseMove.style.background = 'var(--bg-color2)';
   gameActive.style.display = 'grid';
 
   // Computer move
-  html = `<img src="./images/icon-${randomMove}.svg" alt="" class="paper-image user-move-icon">
-  `;
-  houseMove.classList.toggle(`${randomMove}`);
-  houseMove.innerHTML = html;
+
+  setTimeout(() => {
+    houseMove.style.background =
+      'linear-gradient(to bottom, rgb(219, 219, 219), white)';
+    html = `<img src="./images/icon-${randomMove}.svg" alt="" class="paper-image user-move-icon hidden">
+    `;
+    houseMove.classList.toggle(`${randomMove}`);
+    houseMove.innerHTML = html;
+  }, 5000);
 
   // Decide winner
-  decideWinner(moveName, randomMove);
-  result.style.display = 'block';
-  pMove = moveName;
-  hMove = randomMove;
+  setTimeout(() => {
+    decideWinner(moveName, randomMove);
+    [pMove, hMove] = [moveName, randomMove];
+
+    // Update score
+    if (scoreCount >= 0) {
+      score.textContent =
+        scoreCount < 10 ? scoreCount.toString().padStart(2, '0') : scoreCount;
+    } else {
+      scoreCount = 0;
+      score.textContent = '00';
+    }
+  }, 7000);
 }
 
 function decideWinner(player, computer) {
-  if(player == 'paper' && computer == 'rock') {
+  if (player == 'paper' && computer == 'rock') {
     resultText.textContent = 'You win';
-  } else if(player == 'rock' && computer == 'scissors') {
+    scoreCount++;
+  } else if (player == 'rock' && computer == 'scissors') {
     resultText.textContent = 'You win';
-  } else if(player == 'scissors' && computer == 'paper') {
+    scoreCount++;
+  } else if (player == 'scissors' && computer == 'paper') {
     resultText.textContent = 'You win';
-  } else if(player == computer) {
+    scoreCount++;
+  } else if (player == computer) {
     resultText.textContent = 'Draw';
+    scoreCount = scoreCount;
   } else {
     resultText.textContent = 'You lose';
+    scoreCount--;
   }
+  result.style.display = 'block';
 }
 
 moveBtn.forEach((move) => move.addEventListener('click', pickMove));
